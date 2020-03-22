1e96ad86a486e0c1a5353da6abc364461b09f6fb
Peter Madsen <peter.madsen784@myci.csuci.edu>
2020-03-22 09:41:40 -0700

    Initial setup of PyCharm workspace

---
+++ scratchpad.py
@@ -0,0 +1,9 @@
+# The first step is to gather all the data in a workable format.
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import PyPDF4
+import requests
+
+print('This data will be super interesting!')
