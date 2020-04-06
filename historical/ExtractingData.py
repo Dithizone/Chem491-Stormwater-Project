ce758f44262fffed9fa5e364c270fc3b1fec499d
Peter Madsen <peter.madsen784@myci.csuci.edu>
2020-04-06 13:48:18 -0700

    Made the method calls in historical/ExtractingData.py look prettier

--- historical/ExtractingData.py
+++ historical/ExtractingData.py
@@ -8,7 +8,21 @@ from UsefulThings import datanames
 garbagefg = open('../data/precsv/garbagefg.txt', 'w')
 for k in datanames:
     with open(f'data/precsv/precsv{k}.txt', 'w', encoding='utf-8', errors='ignore') as datafile:
-        sepraw = open(f'data/raw/{k}.txt', 'r', encoding='utf-8-sig', errors='ignore').read().replace('\uf076', '||||').replace('\uf0e0', '||||').replace('\u2070', '||||').replace('\u0d4c', '||||').replace('\u0d45', '||||').replace('\u0b3b', '||||').replace('ଶ଴ସି௣ு', '||||').replace('ுି', '||||').replace('ଶ଴ସ', '||||').replace('\u0be3', '||||').replace('\u05db', '||||').replace('\u123a', '||||').replace('\u2265', '||||').split(sep='\n')
+        sepraw = open(f'data/raw/{k}.txt', 'r', encoding='utf-8-sig', errors='ignore').read()\
+            .replace('\uf076', '||||')\
+            .replace('\uf0e0', '||||')\
+            .replace('\u2070', '||||')\
+            .replace('\u0d4c', '||||')\
+            .replace('\u0d45', '||||')\
+            .replace('\u0b3b', '||||')\
+            .replace('ଶ଴ସି௣ு', '||||')\
+            .replace('ுି', '||||')\
+            .replace('ଶ଴ସ', '||||')\
+            .replace('\u0be3', '||||')\
+            .replace('\u05db', '||||')\
+            .replace('\u123a', '||||')\
+            .replace('\u2265', '||||')\
+            .split(sep='\n')
         for i in sepraw:
             eventpresent = 0
             for j in fevents:
