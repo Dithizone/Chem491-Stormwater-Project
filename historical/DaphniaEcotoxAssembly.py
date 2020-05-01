dac8372029a05faf17a7a05e65bd4ae42feca3fb
Peter Madsen <peter.madsen784@myci.csuci.edu>
2020-04-30 21:31:15 -0700

    Shaped Sonoma County until an impasse

---
+++ historical/DaphniaEcotoxAssembly.py
@@ -0,0 +1,65 @@
+# This will explore the ECOTOX data Frankie gathered, found
+# in data/ecotox/aquatic report; Daphnia Magna.csv
+
+# This did its job admirably!
+
+import pandas as pd
+import matplotlib.pyplot as plt
+from UsefulThings import NewData
+
+columnsIwant = ['Test Number',
+                'CAS Number',
+                'Chemical Name',
+                'Test Method',
+                'Species Scientific Name',
+                'Organism Lifestage',
+                'Organism Age Mean',
+                'Organism Age Min',
+                'Organism Age Max',
+                'Age Units',
+                'Exposure Type',
+                'Media Type',
+                'Test Location',
+                'Number of Doses',
+                'Conc 1 Type (Standardized)',
+                'Conc 1 Mean (Standardized)',
+                'Conc 1 Min (Standardized)',
+                'Conc 1 Max (Standardized)',
+                'Conc 1 Units (Standardized)',
+                'Effect',
+                'Effect Measurement',
+                'Endpoint',
+                'Observed Duration Mean (Days)',
+                'Observed Duration Min (Days)',
+                'Observed Duration Max (Days)',
+                'Observed Duration Units (Days)',
+                'Test Type',
+                'Temperature Mean',
+                'Temperature Min',
+                'Temperature Max',
+                'Temperature Units',
+                'Hardness Mean',
+                'Hardness Min',
+                'Hardness Max',
+                'Hardness Units',
+                'pH Mean',
+                'pH Min',
+                'pH Max',
+                'Author',
+                'Title',
+                'Publication Year']
+
+ecotoxdata = pd.read_csv('../data/ecotox/2020.4.30DaphniaAquaticReport.txt', sep='|')
+for i in ecotoxdata.columns.values:  # Shows unique entries and total entries (i.e. are there holes?)
+    print(f'{i}\t\tUniques: {len(ecotoxdata[i].unique())}\t\tCount: {ecotoxdata[i].count()}')
+
+print('\n------- The dataframe (hopefully) ------- ')
+ecotoxdataframe = ecotoxdata[columnsIwant]
+
+# Selecting for just data on Neonates
+ecotoxdataframe = ecotoxdataframe[ecotoxdataframe['Organism Lifestage'] == 'Neonate']\
+    .sort_values(by='Chemical Name', ascending=False).reset_index()
+print(ecotoxdataframe.shape)  # (1007, 41)
+
+# Saving as a file
+ecotoxdataframe.to_csv(path_or_buf='../data/ecotox/DaphniaECOTOX.csv')
--- tubbsfire.py
+++ tubbsfire.py
@@ -1,21 +1,61 @@
-# This will figure out what's in the data/tubbsfire/ceden_data
+# This will figure out what's in the data/tubbsfire/Sonoma County Data 16-19.txt
 # file and how useful it will be.
-
-import pandas as pd
-import matplotlib.pyplot as plt
-from UsefulThings import NewData
 
 # Fire data for Tubbs and others can be found at https://rmgsc.cr.usgs.gov/outgoing/GeoMAC/
 # Fires in data/shapefiles were chosen by cross-referencing fires with
 # those from Ventura county according to e.g. https://en.wikipedia.org/wiki/2017_California_wildfires
 
-tubbsdata = NewData('data/tubbsFire/ceden_data_20200417023345.txt', sep='|')
+import pandas as pd
+import matplotlib.pyplot as plt
+from UsefulThings import NewData
 
-# analytes = tubbsdata.showuniques('Analyte')
-columns = tubbsdata.columns
-# units = tubbsdata.showuniques('Unit')
+tubbsdata = pd.read_csv('data/tubbsFire/Sonoma County Data 16-19.txt', sep='|', dtype={'Result': float})
+# Wow so, one of the entries for Total Dissolved Solids had a Result value
+# of .289.6 which Pandas treated as a string in the middle of a whole
+# column of floats which made memory usage HUGE. So large, in fact, that
+# PyCharm would lock up as it began processing the g/L sections and end
+# with a memory error. I tried erasing previous dataframes mid-script to
+# free memory (if that even works?) but once I found that typo and fixed
+# it, everything runs amazingly fast.
 
-print(columns)
-tubbsdata.showuniques('SampleDate')  # 2018-02-14 to 2018-11-23, oh no
-tubbsdata.showuniques('StationName')
-tubbsdata.showuniques('StationCode')
+for i in tubbsdata.columns.values:
+    print(f'{i}\t\tUniques: {len(tubbsdata[i].unique())}\t\tCount: {tubbsdata[i].count()}')
+
+print(f'\n------- Units -------')
+print(tubbsdata['Unit'].unique())
+print(f'tubbsdata shape is {tubbsdata.shape}')
+
+# Pulling out just ng/L, ug/L, mg/L, and g/L
+tubbsdata_ng = tubbsdata[tubbsdata['Unit'] == 'ng/L']
+
+tubbsdata_ug = tubbsdata[tubbsdata['Unit'] == 'ug/L']
+tubbsdata_ug['Result'] = tubbsdata_ug['Result'] * 1000  # Converts from ug/L to ng/L
+tubbsdata_ug['Unit'] = 'ng/L'  # Updates units label to ng/L
+
+tubbsdata_mg = tubbsdata[tubbsdata['Unit'] == 'mg/L']
+tubbsdata_mg['Result'] = tubbsdata_mg['Result'] * 1000000  # Converts from mg/L to ng/L
+tubbsdata_mg['Unit'] = 'ng/L'
+
+tubbsdata_g = tubbsdata[tubbsdata['Unit'] == 'g/L']
+tubbsdata_g['Result'] = tubbsdata_g['Result'] * 1000000000  # Converts from g/L to ng/L
+tubbsdata_g['Unit'] = 'ng/L'
+
+TheTubbsData = tubbsdata_ng.append(tubbsdata_ug, ignore_index=True)\
+    .append(tubbsdata_mg, ignore_index=True)\
+    .append(tubbsdata_g, ignore_index=True)  # Put all the data back together
+
+print('Pandas disapproves of how I did unit conversions so it\'s giving a "SettingWithCopyWarning" error '
+      'which this print command will now push out of the Run window.\n\n\n\n')
+
+print(f'TheTubbsData shape is {TheTubbsData.shape}')
+
+print(f'\n------- StationNames ({len(TheTubbsData["StationName"].unique())}) -------')
+# for j in TheTubbsData['StationName'].unique():
+#     print(j)
+
+print(f'\n------- StationCode ({len(TheTubbsData["StationCode"].unique())}) -------')
+# for k in TheTubbsData["StationCode"].unique():
+#     print(k)
+
+# TODO: Determine which stations we want to use
+# because omg 82 is waaay too many
